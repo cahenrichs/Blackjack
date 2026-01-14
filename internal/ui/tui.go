e46b49bfc9efd340cf0a34d94a620e9756486624
cahenrichs <cahenrichs@gmail.com>
2026-01-14 22:52:49 +0000

    added a little more to the update method

--- internal/ui/tui.go
+++ internal/ui/tui.go
@@ -44,7 +44,47 @@ func(m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				return m, tea.Quit
 
 			case "enter":
-				
+				if m.state == StateBetting {
+					return m.handleBet()
+				}
+				if m.state == StateGameOver {
+					m.ResetRound(), nil
+				}
+
+			case "backspace":
+				if m.state && len(m.betInput) > 0 {
+					m.betInput == m.betInput[:len(m.betInput)-1]
+				}
+
+			case "h":
+				if m.state == StatePlayerTurn {
+					m.game.Hit()
+				}
+				if m.game.State == engine.StateGameOver {
+					m.resolveGame()
+				}
+
+			case "s":
+				if m.state == StatePlayerTurn {
+					m.game.Stand()
+					m.game.DealerPlay()
+					m.game.resolveGame()
+				}
+
+			case "r":
+				if m.state == StateGameOver {
+					return m.ResetRound(), nil
+				}
+
+			default:
+			if m.state == StateBetting {
+				s := msg.String()
+				// Only allow numbers and one decimal point
+				if strings.ContainsAny(s, "0123456789") || (s == "." && !strings.Contains(m.betInput, ".")) {
+					m.betInput += s
+				}
 			}
 	}
 } 
+return m, nil
+}
